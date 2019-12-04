2e2d7ab7dd8e2c1ae224b4ab776440e507ea9ba1
Daniel Petersen <dlfp16@student.aau.dk>
2019-12-04 16:39:10 +0100

    Added 3d text

---
+++ Assets/TurnlikeCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore;
+
+public class TurnlikeCamera : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.gameObject.transform.rotation = GameObject.FindGameObjectWithTag("MainCamera").transform.rotation;
+    }
+}
